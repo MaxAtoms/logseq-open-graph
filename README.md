aad89428dae428c5f7972cddd0b4c10c51b572d5
MaxAtoms <7847075+MaxAtoms@users.noreply.github.com>
2023-01-29 23:30:14 +0100

    Introduce TypeScript

---
+++ README.md
@@ -0,0 +1,5 @@
+# logseq-open-graph
+
+First prototype of a Logseq plugin which is designed to fetch Open Graph metadata for a given link.
+
+![Short demo](./demo.mp4)
--- index.ts
+++ index.ts
@@ -1,27 +1,36 @@
-function main() {
-  logseq.Editor.registerSlashCommand("Open Graph", async () => {
+import '@logseq/libs'
+
+async function fetchOGImage(content: string) : Promise<string> {
+    const response = await fetch(content);
+    const contentType = response.headers.get("content-type");
+
+    if(contentType == null || !contentType.includes("text/html")) {
+        throw new Error("Loaded URL is not a valid HTML page");
+    }
+
+    const html = await response.text();
+    const parser = new DOMParser();
+    const htmlHead = parser.parseFromString(html, "text/html").head;
+    const metaTag = htmlHead.querySelector(`meta[property='og:image']`);
+    const ogImage = metaTag ? metaTag.getAttribute("content") : null;
+
+    if(ogImage == null) {
+        throw new Error("Page does not contain image");
+    }
+
+    return ogImage;
+}
+
+async function main() {
+  logseq.Editor.registerSlashCommand("Add Open Graph image", async () => {
     const block = await logseq.Editor.getCurrentBlock();
-    const content = block.content;
-    logseq.App.showMsg(`Loading link: ${content}`);
-    var image = await fetch(content)
-      .then((response) => {
-        const contentType = response.headers.get("content-type");
-        if (!contentType.includes("text/html")) {
-          logseq.App.showMsg("Invalid content-type");
-        }
-        return response.text();
-      })
-      .then((html) => {
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(html, "text/html").head;
-        const meta = doc.querySelector(`meta[property='og:image']`);
-        const ogImage = meta ? meta.getAttribute("content") : null;
-        logseq.App.ShowMsg(`Image link is: ${ogImage}`);
-        return ogImage;
-      })
-      .catch((error) => {
-        logseq.App.showMsg("Could not load the webpage");
-      });
+    const content = block?.content;
+
+    if(block == null || content == null) {
+        throw new Error("Block does not contain content");
+    }
+
+    const image = await fetchOGImage(content);
 
     var string=`![${content}](${image})`
     await logseq.Editor.updateBlock(block.uuid, string);
